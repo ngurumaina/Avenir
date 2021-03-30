1f4b8a486d70bb6ea2d42ac9242bd9ca8c2a1034
ngurumaina <ngurumaish91@gmail.comgit config --global user.email ngurumaish91@gmail.com>
2021-03-30 19:19:12 +0300

    First commit

---
+++ main.R
@@ -0,0 +1,18 @@
+
+install.packages("blogdown")
+#blogdown::install_hugo
+#install.packages("knitr")
+library(blogdown)
+
+setwd("C:/Users/DELL/Documents/R/Avenir")
+
+
+#blogdown::new_site(theme = "gethugothemes/omega-hugo")
+
+blogdown::serve_site()
+
+
+
+
+
+
